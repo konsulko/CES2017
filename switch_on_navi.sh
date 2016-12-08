5df5b735697e6dc0177c3b2386dcee2d0eb1ed43
Tasuku Suzuki <tasuku.suzuki@qt.io>
2016-12-08 21:23:16 +0900

    clean up CES2017 repository
    every application should work standalone with the QtQuickControls2 style
    and AGL.Demo.Controls
    
    Change-Id: Ia1ebb5b8c7ad14b52b6378c8ac4c1fa2ab8dc710
    Signed-off-by: Tasuku Suzuki <tasuku.suzuki@qt.io>

--- switch_on_navi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# this script would be called from QML demo UI when a button of map is clicked
-
-if [ $AGL_NAVI = "TRUE" ]; then
-  export XDG_RUNTIME_DIR=/run/user/0
-  /usr/bin/LayerManagerControl set layer 1000 render order $SURFACE_ID_QMLSCENE,$SURFACE_ID_NAVI
-fi
--- switch_to_ivi-shell.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd /etc/xdg/weston
-mv weston.ini weston.ini.desktop-shell
-cp /usr/AGL/CES2017/weston.ini.ivi-shell .
-ln -sf weston.ini.ivi-shell weston.ini
-systemctl restart weston.service
